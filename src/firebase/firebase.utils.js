5698167f448a64b69dfc75945b6c4bae6880f6a5
T1m0r <j.kreutner@gmx.at>
2021-04-04 10:42:44 +0200

    Finished signIn handleSubmit method-> login with email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -33,6 +33,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 			console.log("Error creating User", error.message);
 		}
 	}
+	return userRef;
 };
 
 firebase.initializeApp(config);
