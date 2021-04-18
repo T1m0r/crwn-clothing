0f7adb52d32a3ea33936c7b5d0799d5d51121ac2
T1m0r <j.kreutner@gmx.at>
2021-04-18 21:49:09 +0200

    updated shop data with new data, updated cart utils to group items in cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -4,3 +4,8 @@ export const toggleCartHidden = () => ({
 	type: CartActionTypes.TOGGLE_CART_HIDDEN,
 	//no payload need for this (payload is not required)
 });
+
+export const addItem = (item) => ({
+	type: CartActionTypes.ADD_ITEM,
+	payload: item,
+});
