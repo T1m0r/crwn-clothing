2d7127930abef005a9c7f82338d88cd2d66c7763
T1m0r <j.kreutner@gmx.at>
2021-04-19 21:03:01 +0200

    Updating our App with the Package reselect, replaces cart selectors with memoized

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
@@ -9,16 +10,21 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = ({ cartItems }) => (
 	<div className='cart-dropdown'>
-		{cartItems.map((cartItem) => (
-			<CartItem key={cartItem.id} item={cartItem} />
-		))}
-
+		<div className='cart-items'>
+			{cartItems.map((cartItem) => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</div>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
+/*const mapStateToProps = ({ cart: { cartItems } }) => ({
 	cartItems,
+});*/
+
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(CartDropdown);
