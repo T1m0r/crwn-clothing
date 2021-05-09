9e8f9ad7afea15a2dd9b99e9582f0723102562a9
T1m0r <j.kreutner@gmx.at>
2021-05-09 23:18:03 +0200

    Moved small static local state to redux (Shop page/Directory)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,29 +1,14 @@
-import React from 'react';
-import SHOP_DATA from './shop.data.js'
+import React from "react";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from "../../components/collections-overview/collection-overview.component";
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        };
-
-    }
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                <div>Shop Page</div>
-                {
-                  collections.map(({id, ...otherCollectionProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionProps}></CollectionPreview>
-                  ))
-                }
-            </div>
-        )
-}
-}
+const ShopPage = () => {
+	return (
+		<div className='shop-page'>
+			<div>Shop Page</div>
+			<CollectionsOverview />
+		</div>
+	);
+};
 
 export default ShopPage;
