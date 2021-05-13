5fd62fca7b692739f58b6d44e99f06698ac8a8b1
T1m0r <j.kreutner@gmx.at>
2021-05-13 19:22:17 +0200

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,13 +4,13 @@ import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
 
 // Translates the collectionID string to a corresponding collection int id for collection page
-const COLLECTION_ID_MAP = {
+/*const COLLECTION_ID_MAP = {
 	hats: 1,
 	sneakser: 2,
 	jackets: 3,
 	womens: 4,
 	mens: 5,
-};
+};*/
 
 const selectShop = (state) => state.shop;
 
@@ -19,10 +19,18 @@ export const selectCollections = createSelector(
 	(shop) => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+	[selectCollections],
+	(collections) => Object.keys(collections).map((key) => collections[key])
+);
+
 export const selectCollection = memoize((collectionUrlParam) =>
-	createSelector([selectCollection], (collections) =>
-		collections.find(
+	createSelector(
+		[selectCollections],
+		// Converted the list to object so searching is way fastr
+		/*collections.find(
 			(collection) => collection.id == COLLECTION_ID_MAP[collectionUrlParam]
-		)
+		)*/
+		(collections) => collections[collectionUrlParam]
 	)
 );
