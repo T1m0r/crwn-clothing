d429fb52b2105e75f9f2915f17c8083b3dc2fff4
T1m0r <j.kreutner@gmx.at>
2021-05-24 14:16:42 +0200

    Removed logger form deployed page

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,12 @@ import rootReducer from "./root-reducer";
 
 // this way we can add more middlewares in the future.
 // logger is one middleware
-const middlewares = [logger];
+const middlewares = [];
+
+// Only use the logger in the development mode
+if (proccess.env.NODE_ENV == "development") {
+	middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
